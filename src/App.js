bd68676aa5707855cfa8d951614b816cc74379fa
Đinh Hoàng Lộc <19520148@gm.uit.edu.vn>
2022-08-12 14:03:49 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import ListCourse from "./components/ListCourse";
-import data from "./constants";
-
-const dataCourse = data.map((item, idx) => {
-  ...item, id: idx+ 1;
-})
 
 function App() {
   return (
